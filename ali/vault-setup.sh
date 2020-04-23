f0a5019089657cf13c4d96a8b45dcc486572b63d
Jacob <jacobm3@gmail.com>
2020-04-22 22:36:17 -0500

    Update vault-setup.sh

--- ali/vault-setup.sh
+++ ali/vault-setup.sh
@@ -5,8 +5,14 @@ export VAULT_NAMESPACE=$ns
 vault auth enable alicloud
 
 # Create a RAM role
-# RAM -> RAM Roles -> Create RAM Role
-vault write auth/alicloud/role/web-role arn='acs:ram::5657185762276978:role/web-admin-role'
+# RAM -> RAM Roles -> Create RAM Role -> Ali svc -> ECS
+# Create one a RAM ECS instance role granting Vault permission to create tokens (Ali secrets engine)
+# Create another RAM ECS instance role for the client so it can authenticate to Vault using
+# its local instance metadata
+
+# client metadata role
+vault write auth/alicloud/role/web-instance role arn='acs:ram::5657185762276978:role/web-instance-role'
+
 
 
 
